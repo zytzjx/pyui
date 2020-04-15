df2c770436539ab18982746da02e9540b56766df
Jeffery Zhang <zytzjx@sina.com>
2020-04-14 20:19:18 -0700

    installpsiserver.sh top

--- installpsiserver.sh
+++ installpsiserver.sh
@@ -1,4 +1,8 @@
-sudo cp ./psirun.service /etc/systemd/system/psirun.service
+if [ $1 = "top" ]; then
+    sudo cp ./psiruntop.service /etc/systemd/system/psirun.service
+else
+    sudo cp ./psirun.service /etc/systemd/system/psirun.service
+fi
 sudo chmod 644 /etc/systemd/system/psirun.service
 sudo systemctl daemon-reload
 sudo systemctl enable psirun.service
